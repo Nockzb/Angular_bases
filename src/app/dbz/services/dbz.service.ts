22f1f898c219b543dea940a05a38fa421c055c76
Nockzb <8761804@iesplayamar.es>
2023-12-12 13:04:22 +0100

    id agregado

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
 import { Personaje } from '../interfaces/personaje.interface';
 
 @Injectable({providedIn: 'root'})
@@ -7,25 +9,29 @@ export class DbzService {
 
     public grupoPersonajes: Personaje[] = [
         {
+        id: uuid(),
         nombre: "Goku",
         fuerza: 10000
         },
         {
+        id: uuid(),
         nombre: "Luna",
         fuerza: 2000
         },
         {
+        id: uuid(),
         nombre: "Vegeta",
         fuerza: 4000
         }
     ];
 
     public onNewPersonaje(pers: Personaje): void {
-        this.grupoPersonajes.push(pers)         
+      const newPersonaje: Personaje = { id: uuid(), ...pers } //Añadir el objeto 'pers' luego del id
+      this.grupoPersonajes.push(newPersonaje)
     }
 
     public onDeletePersonaje(index: number): void {
-        console.log(this.grupoPersonajes[index]);        
+        console.log(this.grupoPersonajes[index]);
         this.grupoPersonajes.splice(index, 1);
     }
 }
